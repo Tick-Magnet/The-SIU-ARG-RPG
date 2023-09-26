1f7cd5400fd049244b420e9afe73994fa15d0902
IsaacAmann <isaacmnn@gmail.com>
2023-09-26 09:08:59 -0500

    Added example encounter JSON definition. Added code to parse JSON encounter definitions

--- RPGServer/src/main/java/com/RPGServer/DialogueStep.java
+++ RPGServer/src/main/java/com/RPGServer/DialogueStep.java
@@ -1,8 +1,21 @@
 package com.RPGServer;
 
+import java.util.ArrayList;
 
 public class DialogueStep implements EncounterStep
 {
+	private ArrayList<DialogueOption> dialogueOptions;
+	
+	public DialogueStep()
+	{
+		dialogueOptions = new ArrayList<DialogueOption>();
+	}
+	
+	public void addDialogueOption(String text, int nextStep)
+	{
+		dialogueOptions.add(new DialogueOption(text, nextStep));
+	}
+	
 	//Return next EncounterStep depending on the selected dialogue option
 	public EncounterStep nextStep()
 	{
@@ -23,5 +36,17 @@ public class DialogueStep implements EncounterStep
 	public StepUpdate getInitialStepUpdate()
 	{
 		return null;
+	}
+	
+	private class DialogueOption
+	{
+		public String text;
+		public int nextStep;
+		
+		public DialogueOption(String text, int nextStep)
+		{
+			this.text = text;
+			this.nextStep = nextStep;
+		}
 	}
 }
--- RPGServer/src/main/java/com/RPGServer/Encounter.java
+++ RPGServer/src/main/java/com/RPGServer/Encounter.java
@@ -2,29 +2,82 @@ package com.RPGServer;
 
 import java.io.File;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.*;
+
+import java.io.IOException;
 import java.util.HashMap;
 
 //Encounter system written in a way so that JSON files can be used to create new game content
 public class Encounter
 {
+	public String encounterName;
+	
 	//Reference to the player who initiated the encounter
 	private UserAccount playerAccount;
 	
 	//Contains all steps of encounter, used so generated steps can refer to steps by name
-	private HashMap<String, EncounterStep> encounterStepsMap;
+	private EncounterStep[] encounterSteps;
+	
+	public EncounterEntity[] entityArray;
 	
 	private EncounterStep currentStep;
 	
 	//Construct encounter based on the JSON file passed
-	public Encounter(UserAccount user, String encounterDefinitionPath)
+	public Encounter(UserAccount user, String encounterDefinitionPath) throws IOException
 	{
 		playerAccount = user;
-		encounterStepsMap = new HashMap<String, EncounterStep>();
+		ObjectMapper objectMapper = new ObjectMapper();
+		entityMap = new HashMap<String, EncounterEntity>();
+		
+		JsonNode rootNode = objectMapper.readTree(new File(encounterDefinitionPath));
 		//Fill fields in Encounter class from JSON tree
-		
-		//
-		
-		
+		encounterName = rootNode.get("encounterName").asText();
+		//Read encounter steps array
+		JsonNode encounterStepsNode = rootNode.get("encounterSteps");
+		encounterSteps = new EncounterStep[encounterStepsNode.size()];
+		JsonNode tempNode;
+		for(int i = 0; i < encounterStepsNode.size(); i++)
+		{
+			//Fill step object in array based on node
+			tempNode = encounterStepsNode.get(i);
+			switch(tempNode.get("stepType").asInt())
+			{
+				//Dialogue step
+				case 0:
+					encounterSteps[i] = new DialogueStep();
+					//Add dialogue options
+					JsonNode optionArrayNode = tempNode.get("dialogueOptions");
+					for(int j = 0; j < optionArrayNode.size(); j++)
+					{
+						//Get current dialogue option from the array
+						JsonNode option = optionArrayNode.get(j);
+						//Have to cast to DialogueStep, stored in array as generic encounter step
+						DialogueStep currentStep = (DialogueStep) encounterSteps[i];
+						currentStep.addDialogueOption(option.get("optionText").asText(), option.get("nextStepIndex").asInt());
+					}
+				break;
+				//Combat step
+				case 1:
+				
+				break;
+			}
+		}
+		//Read encounter entities array
+		JsonNode entityArrayNode = rootNode.get("encounterEntities");
+		entityArray = new EncounterEntity[entityArrayNode.size()];
+		for(int i = 0; i < entityArray.length; i++)
+		{
+			//Get current entity from array node
+			tempNode = entityArrayNode.get(i);
+			//Fill EncounterEntity object fields from JSON file
+			entityArray[i] = new EncounterEntity();
+			entityArray[i].name = tempNode.get("name").asText();
+			entityArray[i].imagePath = tempNode.get("imagePath").asText();
+			entityArray[i].strength = tempNode.get("strength").asInt();
+			entityArray[i].dexterity = tempNode.get("dexterity").asInt();
+			entityArray[i].constitution = tempNode.get("constitution").asInt();
+			entityArray[i].intelligence = tempNode.get("intelligence").asInt();
+		}
 	}
 	
 	public StepUpdate postStepUpdate(StepUpdate update)
@@ -41,6 +94,7 @@ public class Encounter
 	private class EncounterEntity
 	{
 		public String name;
+		public String imagePath;
 		//Stats
 		public int strength;
 		public int dexterity;
